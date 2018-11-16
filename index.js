30349d2d6cb994b98b523353dcf5673e2f4be3aa
Nir Adler <niradler55@gmail.com>
2018-11-16 13:12:43 +0200

    override not testedf

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ function Server(config) {
     this.url = config.url;
     this.actions = config.actions;
     for (var key in  this.actions) {
-        Server.prototype[key] = function(param,data) {
+        Server.prototype[key] = function(param,data,override) {
             var host = this.url.concat(this.actions[key].path);
             var find_var = host.indexOf(':');
             if (find_var>-1 && arguments.length>0) {
                 for (var i = 0; i < arguments.length; i++) {
-                    var name=Object.getOwnPropertyNames(arguments[i])[0],value=arguments[i][name];
+                    var name=Object.getOwnPropertyNames(arguments[i])[0];
+                    var value=arguments[i][name];
                     host = host.replace(':' + name,value)
                 }
             }
             var options ={
-                method:  this.actions[key].method,
+                method: this.actions[key].method,
                 url: host,
             }
             if(['post','put'].indexOf(this.actions[key].method.toLowerCase())>-1)
             options.data=data?data:{};
+            if (override) {
+                options = Object.assign(options,override)
+            }
+
             return axios(options);
         }
     }
